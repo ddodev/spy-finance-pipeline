e6d3f34d5134c35162dc3be5a190b9c78ddc0cc3
ddodev <danieldavidolivares@gmail.com>
2026-04-13 20:21:40 -0500

    Update main.tf
    got mixed up with variable.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,38 @@
-variable "project_id" {
-  description = "GCP Project ID"
-  type        = string
-  default     = "extended-bongo-493113-m0"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0" 
+    }
+  }
 }
 
-variable "region" {
-  description = "Region for GCP resources"
-  type        = string
-  default     = "us-central1"
+provider "google" {
+  # For local runs
+   #credentials = file("credentials.json") 
+  project = var.project_id
+  region  = var.region
 }
 
-variable "location" {
-  description = "Project Location"
-  type        = string
-  default     = "US"
+# THE DATA LAKE
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true 
+
+  lifecycle_rule {
+    condition {
+      age = 30 # Files deleted after 30 days 
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
-variable "bq_dataset_name" {
-  description = "My BigQuery Dataset Name"
-  type        = string
-  default     = "market_data_raw"
-}
-
-variable "gcs_bucket_name" {
-  description = "My Storage Bucket Name"
-  type        = string
-  default     = "finance_data_lake_daniel_2026"
+# THE DATA WAREHOUSE
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = var.bq_dataset_name
+  project    = var.project_id
+  location   = var.location
 }
